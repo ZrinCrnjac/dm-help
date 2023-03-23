50a7d87f7a9b3dccacd0453b4e933133ff88b76d
Zrin <zrin.crnjac@gmail.com>
2023-03-23 14:29:15 +0100

    Fix fk errors

--- database/migrations/2023_03_23_122637_create_notes_table.php
+++ database/migrations/2023_03_23_122637_create_notes_table.php
@@ -15,8 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('body');
-            $table->foreignId('character_id')->constrained();
-            $table->foreignId('location_id')->constrained();
             $table->timestamps();
         });
     }
