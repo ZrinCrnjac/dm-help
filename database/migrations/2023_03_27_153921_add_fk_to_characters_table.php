b4a1be57c91a9f1c795a81e0f1e88cc532d84762
Zrin <zrin.crnjac@gmail.com>
2023-03-27 17:40:09 +0200

    Add new table for sublocations

---
+++ database/migrations/2023_03_27_153921_add_fk_to_characters_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('characters', function (Blueprint $table) {
+            $table->foreignId('sublocation_id')->nullable()->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('characters', function (Blueprint $table) {
+            $table->dropForeign(['sublocation_id']);
+            $table->dropColumn('sublocation_id');
+        });
+    }
+};
